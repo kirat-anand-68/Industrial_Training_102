81177fbf59bc9c235e970fff7b4055df72bc61aa
Kirat Anand <167013352+kirat-anand-68@users.noreply.github.com>
2025-09-30 22:05:11 +0530

    Update restaurantdbproject day10.sql

--- restaurantdbproject day10.sql
+++ restaurantdbproject day10.sql
@@ -62,3 +62,50 @@ select category,avg(price) as No_of_dishes
 from menu_items
 group by  category
 
+
+
+select * from order_details;
+
+-- what is the data range of the table
+select Max(order_date), min(order_date) from order_details;
+-- how many orders were made within this range
+select count(distinct order_id) from order_details; 
+-- how many items were order with in  this range
+select count(*) from order_details;
+select 
+order_id, 
+count(item_id) as Num_items
+from order_details
+group by order_id
+order by Num_items desc;
+
+-- how many orders has more than 12 items
+select count(*) from
+(select 
+order_id, 
+count(item_id) as Num_items
+from order_details
+group by order_id
+having Num_items > 12) as Num_orders
+
+
+select * from menu_items;
+select * from order_details;
+
+select * from order_details od left join menu_items mi
+on od.item_id= mi.menu_item_id;
+-- i want to keep the all the transactions so that i have to mek the join as the left join
+select item_name, category,count(order_details_id) as num_purchases
+from order_details od left join menu_items mi
+on od.item_id= mi.menu_item_id
+group by item_name , category
+order by num_purchases;
+-- top 5 order that spent the most money
+select order_id,
+sum(price) as Total_spend
+from order_details od left join menu_items mi
+on od.item_id= mi.menu_item_id
+group by order_id 
+order by Total_spend desc
+limit 5
+
